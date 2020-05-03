135a8144ee172458aed66186d132c5f9c065d797
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-05-03 17:37:40 +0545

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/header";
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
-      KIRAN
+      <Header />
     </div>
   );
 }
